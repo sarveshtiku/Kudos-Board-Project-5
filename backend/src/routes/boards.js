13703f1845879cb1db07f8ccba350a4868356c73
Sarvesh Tiku <sarveshtiku@gmail.com>
2025-07-02 11:28:38 -0700

    Added middleware auth to routes

--- backend/src/routes/boards.js
+++ backend/src/routes/boards.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const auth = require('../middleware/auth');
 const router = express.Router();
 const controller = require('../controllers/boardControllers');
 
--- backend/src/routes/comments.js
+++ backend/src/routes/comments.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const controller = require('../controllers/commentControllers');
 
 // Get all comments for a card
